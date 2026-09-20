263724acc7b253ec4f81fee9bfc643fc8f2c2ba5
bizu <buze101@yahoo.com>
2026-09-20 17:27:28 -0500

    add all the answer files

---
+++ backend/src/api/answer/routes/answer.routes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { authenticateUser } from "../../../middleware/authentication.js";
+import {
+  createAnswerController,
+  voteAnswerController,
+} from "../controller/answer.controller.js";
+import {
+  createAnswerValidation,
+  voteAnswerValidation,
+} from "../validations/answer.validation.js";
+
+const router = express.Router();
+
+/**
+ * @route POST /api/answers
+ * @desc Post an answer to a question
+ * @access Protected
+ */
+router.post(
+  "/",
+  authenticateUser,
+  createAnswerValidation,
+  createAnswerController,
+);
+
+/**
+ * @route POST /api/answers/:answerId/vote
+ * @desc Upvote / downvote / revoke a vote on an answer
+ * @access Protected
+ */
+router.post(
+  "/:answerId/vote",
+  authenticateUser,
+  voteAnswerValidation,
+  voteAnswerController,
+);
+
+export default router;
